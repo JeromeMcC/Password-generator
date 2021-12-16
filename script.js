5f44dca4acac0add177a2c336493100d89edb9bd
Jerome McClarin <Jerome.Mcclarin@gmail.com>
2021-12-16 17:59:11 -0500

    alerts established

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercaseArray = ["a","b","c","d"];
-var numberArray = ["1","2","3","4"];
-var uppercaseArray = ["A","B","C","D"];
-var symbolArray =["!","@","#","$"]
-var allDigits = [...lowercaseArray, ...numberArray, ...uppercaseArray, ...symbolArray]
+var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numberArray = ["0","1","2","3","4","5","6","7","8","9"];
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var symbolArray =["!","@","#","$","%","^","&","*","+","-"];
 
 function generatePassword() 
 {
-  var CharNum= window.prompt("How many characters do you want?")
-  var uppercase = window.prompt("Do you want Uppercase?");
-  var lowercase = window.prompt("Do you want Lowercase?");
-  var numbers = window.prompt("Do you want Numbers?");
-  var symbols = window.prompt("Do you want Symbols?");
-
-
-  if (uppercase === "yes" && lowercase === "yes" && numbers === "yes" && symbols === "yes"){ console.log (allDigits)
-  } else { console.log (numberArray)}
+  var CharNum= parseInt(window.prompt("How many characters do you want? enter a number between 8 and 128"));
+if (Number.isNaN(CharNum)){
+  alert("enter valid number")
+  return null 
 }
-  //for(var i=0; i <= CharNum; i++){}
+if ( CharNum <8 || CharNum > 128){ 
+alert ("enter a number between 8 - 128")
+return null
+}
+
+  var uppercase = window.confirm("Do you want Uppercase?");
+  var lowercase = window.confirm("Do you want Lowercase?");
+  var numbers = window.confirm("Do you want Numbers?");
+  var symbols = window.confirm("Do you want Symbols?");
+
+
+  if (uppercase === false && lowercase === false && numbers === false && symbols === false){ 
+  alert("must select criteria")
+return null
+} 
+ 
+ 
 
 // Write password to the #password input
 function writePassword() {
