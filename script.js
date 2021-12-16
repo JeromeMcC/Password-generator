05ba1eee840148ab378885c349b449d92cdc5534
Jerome McClarin <Jerome.Mcclarin@gmail.com>
2021-12-16 11:56:58 -0500

    mistakes were made

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var CharNum= window.prompt("How many characters do you want?")
+  var uppercase = window.prompt("Do you want Uppercase?");
+  var lowercase = window.prompt("Do you want Lowercase?");
+  var numbers = window.prompt("Do you want Numbers?");
+  var symbols = window.prompt("Do you want Symbols?");
+  var All = 
+
+  //if (uppercase === "yes" && lowercase === "yes" && numbers === "yes" && symbols === "yes"){}
+  //for(var i=0; i <= CharNum; i++){}
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
